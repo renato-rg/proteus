3ee81a238ab069b7d8cf78c3554beb8516156a2f
José Renato Ramos González <renato.etc.etc.etc@gmail.com>
2017-02-27 22:39:52 +0100

    first components added, closes #3, closes #5

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,64 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import {} from './styles/global.css'
+import {header, nav, section} from './styles/global.css'
+import TabsWrapper from './components/TabsWrapper.jsx'
+
+// Mockup data
+const tabs = [
+    "Tab x",
+    "Tab y",
+    "Tab z"
+]
+const contents = [
+    "Tab x's contents",
+    "Tab y's contents",
+    "Tab z's contents"
+]
 
 export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            loaded: false
+        }
+        this.getMyTabsFromHeader = this.getMyTabsFromHeader.bind(this);
+        this.myTabsHeader;
+    }
+
+    componentWillMount() {
+        this.myTabsHeader = (<div>Loading...</div>)
+    }
+
+    componentDidMount() {
+        this.getMyTabsFromHeader();
+    }
+
+    getMyTabsFromHeader() {
+        setTimeout(function (){
+            this.myTabsHeader = (<TabsWrapper tabs={tabs} contents={contents}/>);
+            this.setState({
+                loaded: true
+            })
+        }.bind(this),2000);
+    }
+
     render() {
         return (
-            <div></div>
+            <div>
+                <header>
+                    {this.myTabsHeader}
+                </header>
+                <main>
+                    <nav>
+                        <div>Tabs</div>
+                        <div>Tree</div>
+                    </nav>
+                    <section>
+                        <div>Tabs</div>
+                        <div>Doc</div>
+                    </section>
+                </main>
+            </div>
         )
     }
 }
